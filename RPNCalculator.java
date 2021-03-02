456da5731742867141451d25b6fdb14b65b6304c
JJeshua <79337598+JJeshua@users.noreply.github.com>
2021-03-01 20:41:23 -0800

    Intellij Update
    Moved code on my end to Intellij. Fixed the way the RPNFunctions class works. Methods within this class now return their values and throw a proper error for the driver program to handle.

--- RPNCalculator.java
+++ RPNCalculator.java
@@ -1,76 +1,129 @@
+package com.company;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class RPNCalculator {
 
-	public static void main(String[] args) {
-		Stack<Double> numberStack = new Stack<>();
-		Scanner scanner = new Scanner(System.in);
-		boolean running = true;
+    public static void main(String[] args) {
+        Stack<Double> numberStack = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+        final String notEnoughItemsMessage = "(ERROR) - Not Enough Items in Stack\n\n";
+        boolean running = true;
 
-		while (running) {
-			System.out.print("Enter operation(s) seperated by a space (use h or ? for help): ");
-			// goes through each operation entered by the user and tries to execute it
-			for (String operation : scanner.nextLine().split(" ")) {
-				// checks if user inputed a number, and if so, it is added to the stack
-				try {
-					RPNFunctions.addToStack(numberStack, Double.parseDouble(operation));
-					// if user did not input a number, checks to see if it is a defined function
-				} catch (NumberFormatException e) {
-					switch (operation) {
-					case "+":
-						RPNFunctions.addition(numberStack);
-						break;
-					case "*":
-						RPNFunctions.multiply(numberStack);
-						break;
-					case "-":
-						RPNFunctions.subtract(numberStack);
-						break;
-					case "/":
-						RPNFunctions.divide(numberStack);
-						break;
-					case "%":
-						RPNFunctions.modulous(numberStack);
-						break;
-					case "m":
-						RPNFunctions.negate(numberStack);
-						break;
-					case "r":
-						RPNFunctions.exchange(numberStack);
-						break;
-					case "d":
-						RPNFunctions.duplicate(numberStack);
-						break;
-					case "p":
-						RPNFunctions.printTop(numberStack);
-						break;
-					case "n":
-						RPNFunctions.printRemoveTop(numberStack);
-						break;
-					case "f":
-						RPNFunctions.printAll(numberStack);
-						break;
-					case "c":
-						RPNFunctions.clear(numberStack);
-						break;
-					case "q":
-						System.out.println("Quitting");
-						running = false;
-						scanner.close();
-						break;
-					case "h":
-						RPNFunctions.help();
-						break;
-					case "?":
-						RPNFunctions.help();
-						break;
-					default:
-						System.out.println("Invalid Operation\n");
-						break;
-					}
-				}
-			}
-		}
-	}
+        while (running) {
+            System.out.print("Enter operation(s) seperated by a space (use h or ? for help): ");
+            /* goes through each operation entered by the user and tries to execute it */
+            for (String operation : scanner.nextLine().split(" ")) {
+                /* checks if user inputted a number, and if so, it is added to the stack */
+                try {
+                    RPNFunctions.addToStack(numberStack, Double.parseDouble(operation));
+                    /* if user did not input a number, checks to see if it is a defined function */
+                } catch (NumberFormatException e) {
+                    switch (operation) {
+                        case "+":
+                            try {
+                                RPNFunctions.addition(numberStack);
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                            }
+                            break;
+                        case "*":
+                            try {
+                                RPNFunctions.multiply(numberStack);
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                        }
+                            break;
+                        case "-":
+                            try{
+                                RPNFunctions.subtract(numberStack);
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                            }
+                            break;
+                        case "/":
+                            try{
+                                RPNFunctions.divide(numberStack);
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                            }
+                            break;
+                        case "%":
+                            try{
+                                RPNFunctions.modulous(numberStack);
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                            }
+                            break;
+                        case "m":
+                            try{
+                                RPNFunctions.negate(numberStack);
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                            }
+                            break;
+                        case "r":
+                            try{
+                                RPNFunctions.exchange(numberStack);
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                            }
+                            break;
+                        case "d":
+                            try{
+                                RPNFunctions.duplicate(numberStack);
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                            }
+                            break;
+                        case "p":
+                            try{
+                                System.out.printf("(TOP)\n%f\n\n\n",RPNFunctions.getTop(numberStack));
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                            }
+                            break;
+                        case "n":
+                            try{
+                                System.out.printf("(TOP)\n%f\n\n\n", RPNFunctions.getRemoveTop(numberStack));
+                            } catch (NoSuchElementException exception) {
+                                System.out.println(notEnoughItemsMessage);
+                            }
+                            break;
+                        /* Prints all the numbers in numberStack to the console */
+                        case "f":
+                            System.out.println("(TOP)");
+                            for (Double number : numberStack) {
+                                System.out.println(number);
+                            }
+                            System.out.println("\n");
+                            break;
+                        case "c":
+                            RPNFunctions.clear(numberStack);
+                            System.out.println("Cleared Stack\n\n");
+                            break;
+                        case "q":
+                            System.out.println("Quitting");
+                            running = false;
+                            scanner.close();
+                            break;
+                        case "h":
+                            for (String option: RPNFunctions.getHelpOptions()) {
+                                System.out.println(option);
+                            }
+                            break;
+                        case "?":
+                            for (String option: RPNFunctions.getHelpOptions()) {
+                                System.out.println(option);
+                            }
+                            break;
+                        default:
+                            System.out.println("Invalid Operation\n");
+                            break;
+                    }
+                }
+            }
+        }
+    }
 }
--- RPNFunctions.java
+++ RPNFunctions.java
@@ -1,139 +1,126 @@
+package com.company;
+
+import java.util.NoSuchElementException;
 import java.util.Stack;
 
 public class RPNFunctions {
-	private static double topItem;
-	private static double secondItem;
-	private static String[] options = new String[] { "(any number) Enter any number to add to the stack",
-			"(+) add the top two items", "(*) multiply the top two items",
-			"(-) subtract the top item from the next item", "(/) integer divide the second item by the top item",
-			"(%) find the integer remainder when dividing the second item by the top item",
-			"(m) unary minus -- negate the top item", "(r) exchange the top two items",
-			"(d) duplicate top item on the stack ", "(p) print the top item ", "(n) print and remove the top item ",
-			"(f) print all the contents of the stack (leaving it intact) ", "(c) clear the stack ", "(q) quit ",
-			"(h or ?) help " };
+    private static double topItem;
+    private static double secondItem;
+    private static String[] options = new String[] { "(any number) Enter any number to add to the stack",
+            "(+) add the top two items", "(*) multiply the top two items",
+            "(-) subtract the top item from the next item", "(/) integer divide the second item by the top item",
+            "(%) find the integer remainder when dividing the second item by the top item",
+            "(m) unary minus -- negate the top item", "(r) exchange the top two items",
+            "(d) duplicate top item on the stack ", "(p) print the top item ", "(n) print and remove the top item ",
+            "(f) print all the contents of the stack (leaving it intact) ", "(c) clear the stack ", "(q) quit ",
+            "(h or ?) help " };
 
-	public static void addToStack(Stack<Double> numbers, double number) {
-		numbers.push(number);
-	}
-	
-	public static void addition(Stack<Double> numbers) {
-		if (numbers.size() >= 2) {
-			topItem = numbers.pop();
-			secondItem = numbers.pop();
-			numbers.push(topItem + secondItem);
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static void addToStack(Stack<Double> numbers, double number) {
+        numbers.push(number);
+    }
 
-	public static void multiply(Stack<Double> numbers) {
-		if (numbers.size() >= 2) {
-			topItem = numbers.pop();
-			secondItem = numbers.pop();
-			numbers.push(topItem * secondItem);
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static void addition(Stack<Double> numbers) {
+        if (numbers.size() < 2) {
+            throw new NoSuchElementException();
+        } else {
+            topItem = numbers.pop();
+            secondItem = numbers.pop();
+            numbers.push(topItem + secondItem);
+        }
+    }
 
-	public static void subtract(Stack<Double> numbers) {
-		if (numbers.size() >= 2) {
-			topItem = numbers.pop();
-			secondItem = numbers.pop();
-			numbers.push(topItem - secondItem);
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static void multiply(Stack<Double> numbers) {
+        if (numbers.size() < 2) {
+            throw new NoSuchElementException();
+        } else {
+            topItem = numbers.pop();
+            secondItem = numbers.pop();
+            numbers.push(topItem * secondItem);
+        }
+    }
 
-	public static void divide(Stack<Double> numbers) {
-		if (numbers.size() >= 2) {
-			topItem = numbers.pop();
-			secondItem = numbers.pop();
-			numbers.push(secondItem / topItem);
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static void subtract(Stack<Double> numbers) {
+        if (numbers.size() < 2) {
+            throw new NoSuchElementException();
+        } else {
+            topItem = numbers.pop();
+            secondItem = numbers.pop();
+            numbers.push(topItem - secondItem);
+        }
+    }
 
-	public static void modulous(Stack<Double> numbers) {
-		if (numbers.size() >= 2) {
-			topItem = numbers.pop();
-			secondItem = numbers.pop();
-			numbers.push(secondItem % topItem);
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static void divide(Stack<Double> numbers) {
+        if (numbers.size() < 2) {
+            throw new NoSuchElementException();
+        } else {
+            topItem = numbers.pop();
+            secondItem = numbers.pop();
+            numbers.push(secondItem / topItem);
+        }
+    }
 
-	public static void negate(Stack<Double> numbers) {
-		if (numbers.size() >= 1) {
-			topItem = numbers.pop();
-			numbers.push(topItem * -1);
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static void modulous(Stack<Double> numbers) {
+        if (numbers.size() < 2) {
+            throw new NoSuchElementException();
+        } else {
+            topItem = numbers.pop();
+            secondItem = numbers.pop();
+            numbers.push(secondItem % topItem);
+        }
+    }
 
-	public static void exchange(Stack<Double> numbers) {
-		if (numbers.size() >= 2) {
-			topItem = numbers.pop();
-			secondItem = numbers.pop();
-			numbers.push(topItem);
-			numbers.push(secondItem);
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static void negate(Stack<Double> numbers) {
+        if (numbers.size() < 1) {
+            throw new NoSuchElementException();
+        } else {
+            topItem = numbers.pop();
+            numbers.push(topItem * -1);
+        }
+    }
 
-	public static void duplicate(Stack<Double> numbers) {
-		if (numbers.size() >= 1) {
-			topItem = numbers.lastElement();
-			numbers.push(topItem);
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static void exchange(Stack<Double> numbers) {
+        if (numbers.size() < 2) {
+            throw new NoSuchElementException();
+        } else {
+            topItem = numbers.pop();
+            secondItem = numbers.pop();
+            numbers.push(topItem);
+            numbers.push(secondItem);
+        }
+    }
 
-	public static void printTop(Stack<Double> numbers) {
-		if (numbers.size() >= 1) {
-			System.out.printf("\n(Top)%d\n", numbers.lastElement());
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static void duplicate(Stack<Double> numbers) {
+        if (numbers.size() < 1) {
+            throw new NoSuchElementException();
+        } else {
+            topItem = numbers.lastElement();
+            numbers.push(topItem);
+        }
+    }
 
-	public static void printRemoveTop(Stack<Double> numbers) {
-		if (numbers.size() >= 1) {
-			System.out.printf("\n%f\n\n", numbers.pop());
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}
-	}
+    public static Double getTop(Stack<Double> numbers) {
 
-	public static void printAll(Stack<Double> numbers) {
-		if (numbers.size() >= 1) {
-			System.out.println("\n(TOP)");
-			for (int i = numbers.size() - 1; i > -1; i--) {
-				System.out.println(numbers.elementAt(i));
-			}
-			System.out.println();
-		} else {
-			System.out.println("(ERROR) - Not Enough Items in Stack\n\n");
-		}		
-	}
+        if (numbers.size() < 1) {
+            throw new NoSuchElementException();
+        } else {
+            return numbers.peek();
+        }
+    }
 
-	public static void clear(Stack<Double> numbers) {
-		numbers.clear();
-		System.out.println("Cleared Stack");
-		System.out.println();
-	}
+    public static double getRemoveTop(Stack<Double> numbers) {
 
-	public static void help() {
-		System.out.println();
-		for (String option : options) {
-			System.out.println(option);
-		}
-		System.out.println();
-	}
+        if (numbers.size() < 1) {
+            throw new NoSuchElementException();
+        } else {
+            return numbers.pop();
+        }
+    }
+
+    public static void clear(Stack<Double> numbers) {
+        numbers.clear();
+    }
+
+    public static String[] getHelpOptions() {
+        return options;
+    }
 }
